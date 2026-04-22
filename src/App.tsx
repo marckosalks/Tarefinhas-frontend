0e7713c99dd01aec04b4a5aaf8d37979287f9440
marckosalks <m4rcos.sales@gmail.com>
2026-04-22 00:30:57 -0300

    feat: menu em todas as telas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home/home"
 import { NewTask } from "./pages/newTask/newTask"
+import { EditTask } from "./pages/editTask/editTask"
+
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/newtask" element={<NewTask/>}/>
+      <Route path="/edittask" element={<EditTask/>}/>
     </Routes>
   );
 }
---
+++ src/components/MenuNav.tsx
@@ -0,0 +1,22 @@
+import { BiTask } from "react-icons/bi";
+import { FaHome, FaRegEdit } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export function MenuNav() {
+  return (
+    <div className="flex w-full justify-center">
+      <div className="rounded-2xl fixed bottom-5 w-2/3 bg-amber-950 text-white flex justify-around items-center h-16">
+        {" "}
+        <Link className="text-2xl" to="/">
+          <FaHome />
+        </Link>
+        <Link className="text-2xl" to="/newtask">
+          <BiTask />
+        </Link>
+        <Link className="text-2xl" to="/edittask">
+          <FaRegEdit />
+        </Link>
+      </div>
+    </div>
+  );
+}
